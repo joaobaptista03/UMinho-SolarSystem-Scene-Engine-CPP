1ed0e4e6dfb442495a00c8b27f14d826ba28e3a3
João Rodrigues <jprocharodrigues2003@gmail.com>
2024-03-02 11:15:58 +0000

    inicio main

---
+++ Phase1/Generator/main.cpp
@@ -0,0 +1,25 @@
+#include <cstring>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+
+        if(strcmp(argv[1],"plane")==0){
+            plane(argv[4],atof(argv[2]), atof(argv[3]));
+        }
+
+        if(strcmp(argv[1],"box")==0){
+            box(argv[4],atof(argv[2]), atof(argv[3]));
+        }
+
+        if(strcmp(argv[1],"sphere")==0){
+            sphere(argv[5],atof(argv[2]),atoi(argv[3]),atoi(argv[4]));
+        }
+
+        if(strcmp(argv[1],"cone")==0){
+            cone(argv[6],atof(argv[2]),atof(argv[3]),atoi(argv[4]),atoi(argv[5]));
+
+        }
+
+    return 0;
+}
+
