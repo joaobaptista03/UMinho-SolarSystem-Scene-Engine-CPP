b08300bbcda12cd817bbf738a58dbf80d8f1d071
João Rodrigues <jprocharodrigues2003@gmail.com>
2024-03-03 22:49:42 +0000

    cone a funcionar

---
+++ Phase1/Generator/figures/cone.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+void generateCone(const std::string& fileName, float radius, float height, int slices, int stacks);
+void cone(char*file, float radius, float height, int slices, int stacks);
