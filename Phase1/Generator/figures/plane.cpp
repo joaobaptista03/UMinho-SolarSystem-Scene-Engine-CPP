3621ea513d949587c024fe5601d9fad6f4e54482
João Rodrigues <jprocharodrigues2003@gmail.com>
2024-03-05 11:42:25 +0000

    alteracoes

--- Phase1/Generator/figures/plane.cpp
+++ Phase1/Generator/figures/plane.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <vector>
 
+
 void generatePlane(const std::string& fileName, float length, int divisions) {
    
     std::string path = "../Output/" + fileName;
@@ -18,13 +19,20 @@ void generatePlane(const std::string& fileName, float length, int divisions) {
     float halfLength = length / 2.0f;
     float divisionSize = length / divisions;
     int verticesPerLine = divisions + 1;
+    float** geral = new float*[(divisions + 1) * (divisions + 1)];
+    int count = 0;
 
     // Write vertices
     for (int i = 0; i <= divisions; ++i) {
         for (int j = 0; j <= divisions; ++j) {
             float x = -halfLength + j * divisionSize;
             float z = -halfLength + i * divisionSize;
-            outFile << x << "," << 0 << "," << z << ","<< "\n";
+            float* ponto = new float[3];
+            ponto[0] = x;
+            ponto[1] = 0;
+            ponto[2] = z;
+            geral[count] = ponto;
+            count++;
         }
     }
 
@@ -37,9 +45,9 @@ void generatePlane(const std::string& fileName, float length, int divisions) {
         int bottomRight = bottomLeft + 1;
 
         // First triangle (counter-clockwise)
-        outFile << topLeft << "," << bottomLeft << "," << bottomRight << "," << "\n";
+        outFile << geral[topLeft][0] << "," << geral[topLeft][1] << "," << geral[topLeft][2]<< " " << geral[bottomLeft][0] << "," << geral[bottomLeft][1] << "," << geral[bottomLeft][2] << " " << geral[bottomRight][0] << "," << geral[bottomRight][1] << "," << geral[bottomRight][2] << "\n";
         // Second triangle (counter-clockwise)
-        outFile << topLeft << "," << bottomRight << "," << topRight << "," << "\n";
+        outFile << geral[topLeft][0] << "," << geral[topLeft][1] << "," << geral[topLeft][2]<< " " << geral[bottomRight][0] << "," << geral[bottomRight][1] << "," << geral[bottomRight][2] << " " << geral[topRight][0] << "," << geral[topRight][1] << "," << geral[topRight][2] << "\n";
         }
     }
 
